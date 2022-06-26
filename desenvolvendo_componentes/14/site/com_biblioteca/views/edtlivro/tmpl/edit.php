04073eb2ddddb0207df9d6f1c1bac11bdcd4c61f
Rafael Berlanda <rafaelberlanda@gmail.com>
2022-06-25 23:17:21 -0300

    Ajustes e correções em relação aos passos 13 e 14.

---
+++ desenvolvendo_componentes/14/site/com_biblioteca/views/edtlivro/tmpl/edit.php
@@ -0,0 +1,48 @@
+<?php
+defined('_JEXEC') or die;
+JHtml::_('behavior.formvalidator');
+?>
+<form action="<?php echo JRoute::_('index.php?option=com_biblioteca&view=edtlivro&layout=edit&id='.(int) $this->item->id); ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
+   <div class="page-header">
+      <h1><?php echo JText::_('COM_BIBLIOTECA_EDICAO_LIVRO') ?></h1>
+   </div>
+   <div class="btn-toolbar">
+      <div class="btn-group">
+         <button type="button" class="btn btn-primary" onclick="Joomla.submitbutton('edtlivro.save')">
+            <i class="icon-new"></i> <?php echo JText::_('COM_BIBLIOTECA_BUTTON_SAVE_AND_CLOSE') ?>
+         </button>
+      </div>
+      <div class="btn-group">
+         <button type="button" class="btn btn-primary" onclick="Joomla.submitbutton('edtlivro.apply')">
+            <i class="icon-new"></i> <?php echo JText::_('JSAVE') ?>
+         </button>
+      </div>
+      <div class="btn-group">
+         <button type="button" class="btn btn-primary" onclick="Joomla.submitbutton('edtlivro.cancel')">
+            <i class="icon-cancel"></i> <?php echo JText::_('JCANCEL') ?>
+         </button>
+      </div>
+   </div>
+   <div class="row-fluid">
+      <div class="span10 form-horizontal">
+         <fieldset>
+            <?php echo JHtml::_('bootstrap.startPane', 'myTab', array('active' => 'details')); ?>
+            <?php echo JHtml::_('bootstrap.addPanel', 'myTab', 'details', empty($this->item->id) ? JText::_('COM_BIBLIOTECA_NEW_LIVRO', true) : JText::sprintf('COM_BIBLIOTECA_EDIT_LIVRO', $this->item->id, true)); ?>
+            <?php foreach ($this->form->getFieldset('myfields') as $field) : ?>
+               <div class="control-group">
+                  <div class="control-label">
+                     <?php echo $field->label; ?>
+                  </div>
+                  <div class="controls">
+                     <?php echo $field->input; ?>
+                  </div>
+               </div>
+            <?php endforeach; ?>
+            <?php echo JHtml::_('bootstrap.endPanel'); ?>
+            <input type="hidden" name="task" value="" />
+            <?php echo JHtml::_('form.token'); ?>
+            <?php echo JHtml::_('bootstrap.endPane'); ?>
+         </fieldset>
+      </div>
+   </div>
+</form>
